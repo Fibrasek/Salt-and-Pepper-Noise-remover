aed6ce7ddfdaad10b6519757299da785c22c6ea1
Fibrasek <gabriel1490@gmail.com>
2013-04-19 13:10:58 -0300

    Update addng a +1 margin to the median
    If the median occurs to be 0 or 255 (white or black) them add +/- 1
    position do the median.

--- img.c
+++ img.c
@@ -4,41 +4,41 @@
 #define MAX 512 
 
 //FUNCOES
-void limpa_imagem(char magic_number[128], int n, int m, int max, int matriz[512][512]);
-void grava_nova(char magic_number[128], int n, int m, int max, int matriz[512][512]);
+void limpa_imagem(char magic_number[128], int n, int m, int max, int matriz[MAX][MAX]);
+void grava_nova(char magic_number[128], int n, int m, int max, int matriz[MAX][MAX]);
 int mediana(int org[]);
 
-int main(){
+int main() {
 	FILE *arq;
 	char fname[MAX_NAME];
 	char magic_number[128];
-	int i,j,n,m,max, M[MAX][MAX];
+	int i, j, n, m, max, M[MAX][MAX];
 
-	/* FRESCURA */
-	printf("Digite o nome do arquivo PGM de entrada: ") ;
-	scanf("%s", fname) ;
+	/* PEGA O NOME DO ARQUIVO */
+	printf("Digite o nome do arquivo PGM de entrada: ");
+	scanf("%s", fname);
 
-	arq = fopen(fname, "r") ;
+	arq = fopen(fname, "r");
 
-	if(arq == NULL) {
-		printf("Erro na abertura do arquivo %s\n", fname) ;
-		return 0 ;
+	if (arq == NULL) {
+		printf("Erro na abertura do arquivo %s\n", fname);
+		return 0;
 	}
 
 	/* LE O NUMERO MAGICO = P2  */
 	fscanf(arq, "%s", magic_number);
-	if(strcmp(magic_number, "P2") != 0) {
-		printf("Arquivo nao e um PGM\n");
-		fclose(arq) ;
-		return 0 ;
+	if (strcmp(magic_number, "P2") != 0) {
+		printf("Arquivo não e um PGM\n");
+		fclose(arq);
+		return 0;
 	}
 	fscanf(arq, "%d %d %d", &m, &n, &max);
 
 	/* LE OS DADOS E ENFIA ELES NUMA MATRIZ */
-	for(i=0; i<=n-1; i++) {
-		for(j=0;j<=m-1;j++){
-	    	fscanf(arq, " %d ", &M[i][j]);
-	    }
+	for (i = 0; i <= n - 1; i++) {
+		for (j = 0; j <= m - 1; j++) {
+			fscanf(arq, " %d ", &M[i][j]);
+		}
 	}
 	fclose(arq);
 
@@ -47,8 +47,8 @@ int main(){
 	return 0;
 }
 
-// LIMPA UMA IMAGEM A PARTIR DO M��TODO DA MEDIANA
-void limpa_imagem(char magic_number[128], int n, int m, int max, int matriz[512][512]){
+// LIMPA UMA IMAGEM A PARTIR DO MÉTODOO DA MEDIANA
+void limpa_imagem(char magic_number[128], int n, int m, int max, int matriz[MAX][MAX]) {
 
 	printf("Magic Number: %s\nN: %d\nM: %d\nMax: %d\n", magic_number, n, m, max);
 
@@ -59,79 +59,59 @@ void limpa_imagem(char magic_number[128], int n, int m, int max, int matriz[512]
 	int vizinhos[9];
 
 	// CONTADORES
-	int i,j;
+	int i, j, k;
 
-	for(i = 1; i < n-1; i++) {
-		for(j = 1; j < m-1; j++) {
-			//CHECA SE i EST�� NA BORDA SUPERIOR
-			//CASO SIM, OS VIZINHOS ACIMA SER��O = 0
-	    	/*if(i == 0){
-	    		vizinhos[0] = 0; vizinhos[1] = 0; vizinhos[2] = 0;
-	    	} else {*/
-	    		vizinhos[0] = matriz[i-1][j-1];
-	    		vizinhos[1] = matriz[i-1][j];
-	    		vizinhos[2] = matriz[i-1][j+1];
-	    	//}
+		for (i = 1; i < n - 1; i++) {
+			for (j = 1; j < m - 1; j++) {
+				vizinhos[0] = matriz[i - 1][j - 1];
+				vizinhos[1] = matriz[i - 1][j];
+				vizinhos[2] = matriz[i - 1][j + 1];
+				vizinhos[3] = matriz[i][j - 1];
+				vizinhos[4] = matriz[i][j];
+				vizinhos[5] = matriz[i][j + 1];
+				vizinhos[6] = matriz[i + 1][j - 1];
+				vizinhos[7] = matriz[i + 1][j];
+				vizinhos[8] = matriz[i + 1][j + 1];
 
-	    	// CHECA SE i EST�� NA BORDA INFERIOR
-	    	// CASO SIM, OS VIZINHOS ABAIXO SER��O = 0
-	    	/*if(i == n-1){
-	    		vizinhos[6] = 0; vizinhos[7]; vizinhos[8] = 0;
-	    	} else {*/
-	    		vizinhos[6] = matriz[i+1][j-1];
-	    		vizinhos[7] = matriz[i+1][j];
-	    		vizinhos[8] = matriz[i+1][j+1];
-	    	//}
-
-			// CHECA SE i EST�� NA BORDA ESQUERDA
-	    	// CASO SIM, OS VIZINHOS A ESQUERDA SER��O = 0
-	    	/*if(j == 0){
-	    		vizinhos[0] = 0; vizinhos[3] = 0; vizinhos[6] = 0;
-	    	} else {*/
-	    		//vizinhos[0] = matriz[i-1][j-1];
-	    		vizinhos[5] = matriz[i][j+1];
-	    		//vizinhos[6] = matriz[i+1][j-1];
-	    	//}
-
-	    	// CHECA SE i EST�� NA BORDA DIREITA
-	    	// CASO SIM, OS VIZINHOS A ESQUERDA SER��O = 0
-	    	/*if(j == m-1){
-	    		vizinhos[2] = 0; vizinhos[5] = 0; vizinhos[8] = 0; 
-	    	} else {
-	    		vizinhos[2] = matriz[i-1][j+1]; 
-	    		vizinhos[5] = matriz[i][j+1]; 
-	    		vizinhos[8] = matriz[i+1][j+1];
-	    	}*/
-
-	    	vizinhos[4] = matriz[i][j];
-
-	    	matriz[i][j] = mediana(vizinhos);
-	    	printf("%d\n", matriz[i][j]);
+				matriz[i][j] = mediana(vizinhos);
+				//printf("%d\n", matriz[i][j]);
+			}
 		}
-	}
-			
+
 	grava_nova(magic_number, n, m, max, matriz);
 }
 
 // ORDENA MATRIZ E RETORNA MEDIANA
-int mediana(int org[]){
+int mediana(int org[]) {
 	int i, aux;
 
-    for(i = 0; i < 9; ++i) {
-        if(org[i]>org[i+1]) {
-            aux = org[i];
-            org[i] = org[i+1];
-            org[i+1] = aux;
-        }
-    }
-    return (org[4]);
+	for (i = 0; i < 9; ++i) {
+		if (org[i] >= org[i + 1]) {
+			aux = org[i];
+			org[i] = org[i + 1];
+			org[i + 1] = aux;
+		}
+	}
+	printf("Mediana: %d\n", org[4]);
+	if(org[4] == 0){
+		return org[3];
+	} else if(org[4] == 255){
+		return org[5];
+	}
+
+
+	//printf("[0]: %d | [1]: %d | [2]: %d | [3]: %d | [4]: %d | [5]: %d | [6]: %d | [7]: %d | [8]: %d\n",
+	//		org[0], org[1], org[2], org[3], org[4], org[5], org[6], org[7], org[8]);
+
+	return (org[4]);
 }
 
 // GRAVA A NOVA IMAGEM LIMPA
-void grava_nova(char magic_number[128], int n, int m, int max, int matriz[MAX][MAX]){
+void grava_nova(char magic_number[128], int n, int m, int max,
+		int matriz[MAX][MAX]) {
 	//printf("%s, %d, %d, %d", magic_number, n, m, max);
 
-	int i,j;
+	int i, j;
 
 	FILE *new_arq;
 
@@ -142,10 +122,10 @@ void grava_nova(char magic_number[128], int n, int m, int max, int matriz[MAX][M
 	fprintf(new_arq, "%d\n", m);
 	fprintf(new_arq, "%d\n", max);
 
-	for(i = 0; i <= n-1; i++) {
-		for(j = 0;j <= m-1; j++){
-	    	fprintf(new_arq, " %d ", matriz[i][j]);
-	    }
+	for (i = 0; i <= n - 1; i++) {
+		for (j = 0; j <= m - 1; j++) {
+			fprintf(new_arq, " %d ", matriz[i][j]);
+		}
 	}
 
 	fclose(new_arq);
